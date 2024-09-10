233a5919ea7f498f46696064a3208b0518aa4537
Val-jimenez <vjimeneza@uninorte.edu.co>
2024-09-09 19:52:34 -0500

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  path: 'https://' // This will be the default path for my GitPages deployment
+  base: 'https://val-jimenez.github.io/3-WEBFE-REACT-VITE-VAL/' // This will be the default path for my GitPages deployment
 })
